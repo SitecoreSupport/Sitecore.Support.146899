0f481c242a2fd6eee275de531c2be15f1c8262a6
Sergey Plashenko <spl@sitecore.net>
2017-11-29 09:43:26 +0200

    Check for null successor was added.

--- src/Sitecore.Support.146899/sitecore/admin/FixContacts.aspx.cs
+++ src/Sitecore.Support.146899/sitecore/admin/FixContacts.aspx.cs
@@ -14,51 +14,61 @@ using Sitecore.Analytics.Data;
 using Sitecore.Analytics.Model;
 using Sitecore.Analytics.Configuration;
 using Sitecore.Data;
+using Sitecore.Diagnostics;
 
 namespace Sitecore.Support
 {
-    public partial class FixContacts : System.Web.UI.Page
+  public partial class FixContacts : System.Web.UI.Page
+  {
+    MongoDbDriver driver = MongoDbDriver.FromConnectionString("analytics");
+    public MongoCursor<BsonDocument> FindContacts()
     {
-        MongoDbDriver driver = MongoDbDriver.FromConnectionString("analytics");
-        public MongoCursor<BsonDocument> FindContacts()
-        {
-            IMongoQuery successorExists = Query.Exists("Successor");
-            IMongoQuery systemExists = Query.Exists("System");
-            IMongoQuery query = Query.And(successorExists, systemExists);
-            return driver.Contacts.FindAs<BsonDocument>(query);
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            var contacts = FindContacts();
-            ContactManager contactManager = Factory.CreateObject("tracking/contactManager", true) as ContactManager;
-            ContactRepositoryBase contactRepository = Factory.CreateObject("tracking/contactRepository", true) as ContactRepositoryBase;
-            foreach (BsonDocument contact in contacts)
-            {
-                var successorId = contact["Successor"];
-                var id = contact["_id"];
-                //remove incorrect Successor and Identifiers fields.
-                driver.Contacts.Update(Query.EQ("_id", id), Update.Unset("Successor").Unset("Identifiers"));
-
-                var survivingContact = contactManager.LoadContactReadOnly(successorId.AsGuid);
-                var dyingContact = contactManager.LoadContactReadOnly(id.AsGuid);
-
-                //Merging contacts
-                contactManager.MergeContacts(survivingContact, dyingContact);
-                contactManager.ReleaseContact(dyingContact.ContactId);
-                contactManager.GetType().GetMethod("RemoveFromSession", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(contactManager, new object[] { dyingContact.ContactId });
-
-                //Obsoleting dying contact
-                LeaseOwner leaseOwner = new LeaseOwner(AnalyticsSettings.ClusterName, LeaseOwnerType.WebCluster);
-                contactRepository.ObsoleteContact(dyingContact, leaseOwner, new ID(survivingContact.ContactId));
-            }
-            Label1.Text = "Completed";
-        }
-
-        protected void Button2_Click(object sender, EventArgs e)
-        {
-            var contacts = FindContacts();
-            Label1.Text = "Found "+ contacts.Count().ToString()+ " corrupted contacts";
-        }
+      IMongoQuery successorExists = Query.Exists("Successor");
+      IMongoQuery systemExists = Query.Exists("System");
+      IMongoQuery query = Query.And(successorExists, systemExists);
+      return driver.Contacts.FindAs<BsonDocument>(query);
     }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+      var contacts = FindContacts();
+      Log.Audit("Fix contacts was started", this);
+      ContactManager contactManager = Factory.CreateObject("tracking/contactManager", true) as ContactManager;
+      ContactRepositoryBase contactRepository = Factory.CreateObject("tracking/contactRepository", true) as ContactRepositoryBase;
+      foreach (BsonDocument contact in contacts)
+      {
+        var successorId = contact["Successor"];
+        var id = contact["_id"];
+
+        var survivingContact = contactManager.LoadContactReadOnly(successorId.AsGuid);
+
+
+        if (survivingContact == null)
+        {
+          Log.Audit("Surviving contact is null " + successorId.AsGuid, this);
+          return;
+        }
+
+        //remove incorrect Successor and Identifiers fields.
+        driver.Contacts.Update(Query.EQ("_id", id), Update.Unset("Successor").Unset("Identifiers"));
+
+        var dyingContact = contactManager.LoadContactReadOnly(id.AsGuid);
+        //Merging contacts
+        contactManager.MergeContacts(survivingContact, dyingContact);
+        contactManager.ReleaseContact(dyingContact.ContactId);
+        contactManager.GetType().GetMethod("RemoveFromSession", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(contactManager, new object[] { dyingContact.ContactId });
+
+        //Obsoleting dying contact
+        LeaseOwner leaseOwner = new LeaseOwner(AnalyticsSettings.ClusterName, LeaseOwnerType.WebCluster);
+        contactRepository.ObsoleteContact(dyingContact, leaseOwner, new ID(survivingContact.ContactId));
+      }
+      Label1.Text = "Completed";
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+      var contacts = FindContacts();
+      Label1.Text = "Found " + contacts.Count().ToString() + " corrupted contacts";
+    }
+  }
 }
