ef178f49cb8b49b51cfcb1e75996aba8b1f1cb3d
Sergey Plashenko <sergey.plashenko@sitecore.com>
2020-06-19 15:59:42 +0300

    Check for successor loops was added.

--- src/Sitecore.Support.146899/sitecore/admin/FixContacts.aspx.cs
+++ src/Sitecore.Support.146899/sitecore/admin/FixContacts.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -29,18 +30,68 @@ namespace Sitecore.Support
       return driver.Contacts.FindAs<BsonDocument>(query);
     }
 
+    private bool CheckIfSuccessorContactCanBeLoaded(BsonValue successorContactId, List<Guid> contacts)
+    {
+        contacts.Add(successorContactId.AsGuid);
+
+        var idQuery = Query.EQ("_id", successorContactId);
+        var contact = driver.Contacts.FindOneAs<BsonDocument>(idQuery);
+
+        if (contact == null)
+        {
+            Log.Warn($"Successor contact does not exist {successorContactId.AsGuid} and can't be loaded", this);
+            return false;
+        }
+
+        if (contact.TryGetValue("Successor", out BsonValue successorValue))
+        {
+            //if (contact.TryGetElement("Identifiers", out BsonElement element) &&
+            //    element.Value.AsBsonDocument.TryGetValue("Identifier", out _))
+            //{
+            //    Log.Warn($"Successor contact has both Successor and Identifiers.Identifier {successorContactId.AsGuid}",
+            //        this);
+            //    return false;
+            //}
+
+            if (contacts.Contains(successorValue.AsGuid))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var id in contacts)
+                {
+                    sb.AppendLine(id.ToString());
+                }
+
+                Log.Warn($"Successor contacts loop is found: {sb}", this);
+                return false;
+            }
+
+            return CheckIfSuccessorContactCanBeLoaded(successorValue, contacts);
+        }
+
+        return true;
+    }
+
     protected void Button1_Click(object sender, EventArgs e)
     {
       var contacts = FindContacts();
       Log.Audit("Fixing contacts was started", this);
       ContactManager contactManager = Factory.CreateObject("tracking/contactManager", true) as ContactManager;
       ContactRepositoryBase contactRepository = Factory.CreateObject("tracking/contactRepository", true) as ContactRepositoryBase;
+      int failedCounter = 0;
+      int successfulCounter = 0;
       foreach (BsonDocument contact in contacts)
       {
         var successorId = contact["Successor"];
         var id = contact["_id"];
 
-        Log.Audit($"Processing contact with id: {id} and Successor {successorId}", this);
+        Log.Audit($"Processing contact with id: {id.AsGuid} and Successor {successorId.AsGuid}", this);
+
+        if (!CheckIfSuccessorContactCanBeLoaded(successorId, new List<Guid>()))
+        {
+            Log.Warn($"Processing contact with id: {id.AsGuid} and Successor {successorId.AsGuid} failed. Skipping it.", this);
+            failedCounter++;
+            continue;
+        }
 
         var survivingContact = contactManager.LoadContactReadOnly(successorId.AsGuid);
 
@@ -63,11 +114,12 @@ namespace Sitecore.Support
         //Obsoleting dying contact
         LeaseOwner leaseOwner = new LeaseOwner(AnalyticsSettings.ClusterName, LeaseOwnerType.WebCluster);
         contactRepository.ObsoleteContact(dyingContact, leaseOwner, new ID(survivingContact.ContactId));
+        successfulCounter++;
       }
 
       Log.Audit("Fixing contacts was finished", this);
       
-      Label1.Text = "Completed";
+      Label1.Text = $"Completed. Failed: {failedCounter}. Successful: {successfulCounter}";
     }
 
     protected void Button2_Click(object sender, EventArgs e)
